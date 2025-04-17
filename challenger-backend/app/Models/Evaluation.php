f84a3b8b4100d807f2b2105f81450aaeb644007f
MARUCCI MAURO NAHUEL <marucci.mauro@gmail.com>
2025-04-17 16:47:25 -0300

    feat(evaluations): add relations teacher and student in Evaluation Model

---
+++ challenger-backend/app/Models/Evaluation.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Evaluation extends Model
+{
+    //
+
+    public function teacher()
+    {
+        return $this->belongsTo(User::class, 'teacher_id', 'id');
+    }
+
+    public function student()
+    {
+        return $this->belongsTo(User::class, 'student_id', 'id');
+    }
+}
