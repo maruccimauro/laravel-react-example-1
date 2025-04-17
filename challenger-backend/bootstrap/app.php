6471c85f29f229c00232c4770f9bfd9fb844f338
MARUCCI MAURO NAHUEL <marucci.mauro@gmail.com>
2025-04-17 18:47:36 -0300

    feat(middleware): register middlewares IsUserAuth and IsTeacher

--- challenger-backend/bootstrap/app.php
+++ challenger-backend/bootstrap/app.php
@@ -1,18 +1,22 @@
 <?php
 
+use App\Http\Middleware\IsTeacher;
+use App\Http\Middleware\isUserAuth;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
+        isUserAuth::class;
+        IsTeacher::class;
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
