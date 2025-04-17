ba147626cabdc6ccc44f500fecfcf7728c409d9d
MARUCCI MAURO NAHUEL <marucci.mauro@gmail.com>
2025-04-17 16:00:56 -0300

    feat(auth): add routes login, register, logout and getuser

--- challenger-backend/routes/api.php
+++ challenger-backend/routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Middleware\isUserAuth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+
+Route::post("/login", [AuthController::class, 'login']);
+Route::post("/register", [AuthController::class, 'register']);
+Route::middleware(isUserAuth::class)->group(function () {
+    Route::post("/logout", [AuthController::class, 'logout']);
+    Route::post("/getuser", [AuthController::class, 'getUser']);
+});
