981bf3e5bbaf7dd9e8e2bd5227be16dca2a1d37e
MARUCCI MAURO NAHUEL <marucci.mauro@gmail.com>
2025-04-17 20:14:17 -0300

    fix(route auth): The 'auth' prefix is ​​added to all routes

--- challenger-backend/routes/api.php
+++ challenger-backend/routes/api.php
@@ -9,11 +9,11 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::post("/login", [AuthController::class, 'login']);
-Route::post("/register", [AuthController::class, 'register']);
+Route::post("/auth/login", [AuthController::class, 'login']);
+Route::post("/auth/register", [AuthController::class, 'register']);
 Route::middleware(IsUserAuth::class)->group(function () {
-    Route::post("/logout", [AuthController::class, 'logout']);
-    Route::post("/getuser", [AuthController::class, 'getUser']);
+    Route::post("/auth/logout", [AuthController::class, 'logout']);
+    Route::post("/auth/getuser", [AuthController::class, 'getUser']);
 
 
     Route::middleware(IsTeacher::class)->group(function () {
