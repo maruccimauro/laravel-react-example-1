d9d9f176d60444437c8dc136247f6006e376baa6
MARUCCI MAURO NAHUEL <marucci.mauro@gmail.com>
2025-04-17 18:13:18 -0300

    feat(evaluation): add EvaluationSeeder and EvaluationFactory in seeder system.

--- challenger-backend/database/seeders/DatabaseSeeder.php
+++ challenger-backend/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Evaluation;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,5 +16,6 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
         $this->call(UserSeeder::class);
+        $this->call(EvaluationSeeder::class);
     }
 }
