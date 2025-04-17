f052acdd1058cdf63074de65d1a07a00c84eb83b
MARUCCI MAURO NAHUEL <marucci.mauro@gmail.com>
2025-04-17 15:31:04 -0300

    feat(auth): add AuthController with register, login, getUser, logout methods

---
+++ challenger-backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Termwind\Components\Hr;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        // validate data
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|min:3|max:50',
+            'email' => 'required|email|min:10|max:255|unique:users,email',
+            'password' => 'required|string|min:8|max:30|confirmed',
+            'role' => 'required|in:student,teacher'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['message' => 'Data validation error.', 'errors' => $validator->errors()], 422);
+        }
+
+        // create user
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => $request->role,
+        ]);
+
+        return response()->json(['message' => "New user created successfully"], 201);
+    }
+
+    public function login(Request $request)
+    {
+        //validate data
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email|min:10|max:255',
+            'password' => 'required|string|min:8|max:30'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['message' => 'Data validation error'], 422);
+        }
+
+        try {
+            $credentials = $request->only(['email', 'password']);
+            $token = JWTAuth::attempt($credentials);
+            if (!$token) {
+                return response()->json(['message' => 'Invalid credentials'], 401);
+            }
+            return response()->json(['message' => 'User logged successfully', 'token' => $token, 'user' => $request->email], 200);
+        } catch (JWTException $error) {
+            return response()->json(['message' => 'Could not create token', 'error' => $error], 500);
+        }
+    }
+
+
+    public function getUser()
+    {
+        $user = Auth::user();
+        return response()->json(['user' => $user], 200);
+    }
+
+    public function loguot()
+    {
+        try {
+            JWTAuth::invalidate(JWTAuth::getToken());
+            return response()->json(['message' => 'Log out successfully'], 200);
+        } catch (JWTException $error) {
+            return response()->json(["message" => 'Failed to logout, token invalid'], 500);
+        }
+    }
+}
